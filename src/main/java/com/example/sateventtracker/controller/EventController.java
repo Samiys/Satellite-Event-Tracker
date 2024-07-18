bbd2fa353095e8f9422e97ddb7bc0ce5949330ae
Sami <samiys005@gmail.com>
2024-07-18 15:43:39 +0200

    adding EventController

---
+++ src/main/java/com/example/sateventtracker/controller/EventController.java
@@ -0,0 +1,10 @@
+package com.example.sateventtracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+
+}
