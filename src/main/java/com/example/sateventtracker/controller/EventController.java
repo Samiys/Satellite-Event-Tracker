674cbb6a70a2419521ed6358b0e7d1e109af52d7
Sami <samiys005@gmail.com>
2024-07-18 19:26:20 +0200

    adding api to create event

--- src/main/java/com/example/sateventtracker/controller/EventController.java
+++ src/main/java/com/example/sateventtracker/controller/EventController.java
@@ -7,4 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/events")
 public class EventController {
 
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping
+    public Event createEvent(@RequestBody Event event) {
+        return eventService.createEvent(event);
+    }
+
 }
--- src/main/java/com/example/sateventtracker/service/EventService.java
+++ src/main/java/com/example/sateventtracker/service/EventService.java
@@ -4,4 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public Event createEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
 }
