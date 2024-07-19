64dd8ddab174efa4a94c550db76c911100ba8a4d
Sami <samiys005@gmail.com>
2024-07-19 17:02:21 +0200

    removing the validation constraints

--- src/main/java/com/example/sateventtracker/model/Event.java
+++ src/main/java/com/example/sateventtracker/model/Event.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,16 +16,8 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull(message = "Date cannot be null")
     private LocalDateTime date;
-
-    @NotEmpty(message = "Description cannot be empty")
     private String description;
-
-    @NotEmpty(message = "Priority cannot be empty")
     private String priority;
-
-    @NotEmpty(message = "Satellite name cannot be empty")
     private String satelliteName;
 }
